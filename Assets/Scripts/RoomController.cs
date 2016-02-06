a4aee9277f3c220f9d739dc73810afbbb1008ce3
Onbeygir <onbeygir@hotmail.com>
2016-02-07 01:31:14 +0200

    DONE

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,20 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RoomController : MonoBehaviour
 {
 
     public PlayerInputController PlayerIC;
-    private Rigidbody _rigidbody;
-    public float Torque;
+    public Transform PlayerPos;
+    private Rigidbody _rigidbody;    
+    public float forceMultiplier;
 
     private bool _ready;
+    private List<Rigidbody> _objects;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    _rigidbody = GetComponent<Rigidbody>();
+	    _rigidbody = GetComponent<Rigidbody>();        
 	    StartCoroutine(ReadyAfterSecs(2f));
+        _objects = new List<Rigidbody>();
+
+	    GameObject[] gos = GameObject.FindGameObjectsWithTag("objs");
+
+	    foreach (GameObject go in gos)
+	    {
+	        _objects.Add(go.GetComponent<Rigidbody>());
+	    }
+
 	}
 	
 	// Update is called once per frame
@@ -23,11 +35,32 @@ public class RoomController : MonoBehaviour
 	    {
 	        return;
 	    }
-        Vector3 torque = transform.up * Torque * -PlayerIC.MouseDelta.x;
-	    Vector3 t2 = transform.right*Torque*PlayerIC.MouseDelta.y;
-	    torque = torque + t2;
-	    _rigidbody.AddRelativeTorque(torque,ForceMode.Acceleration);
+        //Vector3 pos = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.x);
+        //
+        //Vector3 torque = transform.up * Torque * -PlayerIC.MouseDelta.x;
+	    //torque += pos;
+	    ////Vector3 t2 = transform.right * Torque * PlayerIC.MouseDelta.y;
+	    ////torque = torque + t2;
+	    //_rigidbody.AddRelativeTorque(torque,ForceMode.Acceleration);
 
+	    float rad = PlayerIC.MouseDelta.x * Mathf.Deg2Rad;
+        Debug.Log(rad);
+	    if (Mathf.Abs(rad) <= 0.2f)
+	    {
+	        return;
+	    }
+
+	    foreach (Rigidbody o in _objects)
+	    {
+	        Vector3 vec = o.position - PlayerPos.position;
+            Vector3 vec90Deg = Quaternion.AngleAxis(rad > 0 ? -1 * 90 : 1 * 90, Vector3.up) * vec;
+            Debug.DrawRay(o.position, vec90Deg, Color.red, 0.01f);
+
+            o.AddForce(vec90Deg * forceMultiplier,ForceMode.VelocityChange);
+
+            //float v = Mathf.Tan(rad) * vec.magnitude;
+	    }
+        
 	}
 
     private IEnumerator ReadyAfterSecs(float s)
