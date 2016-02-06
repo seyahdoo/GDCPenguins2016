3d8a74f919915da283891e001cce7b10480ba1b8
Onbeygir <onbeygir@hotmail.com>
2016-02-07 01:45:23 +0200

    asdasdasd

--- Assets/Scripts/RoomRotater.cs
+++ Assets/Scripts/RoomRotater.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class RoomController : MonoBehaviour
+public class RoomRotater : MonoBehaviour
 {
 
     public PlayerInputController PlayerIC;
