438f4d9adf00bae3c864e85ae706d16d3bb151df
Onbeygir <onbeygir@hotmail.com>
2016-02-07 18:17:44 +0200

    asd

---
+++ Assets/Scripts/PuzzleManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleManager : MonoBehaviour
+{
+
+    public Transform [] RequiredStuff;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
