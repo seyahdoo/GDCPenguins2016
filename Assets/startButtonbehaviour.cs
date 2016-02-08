250ae59c7d622379e633034aebfa5736aabf5cc8
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2016-02-08 04:56:25 +0200

    asd

---
+++ Assets/startButtonbehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startButtonbehaviour : MonoBehaviour {
+
+
+
+
+    public void PressedStart()
+    {
+
+        Application.LoadLevel("RoomFinal");
+
+    }
+
+}
