91b74acb6e9e85f347ae11da141f6f638d5baa43
Seyyid Ahmed Doğan <seyyidahmed.dogann@gmail.com>
2016-02-08 02:15:25 +0200

    asd

---
+++ Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour {
+
+
+    private InterfaceManager _im;
+    public InterfaceManager Instance
+    {
+
+        get {
+
+            if(_im == null)
+            {
+                _im = this;
+            }
+
+            return _im;
+        }
+
+    }
+
+
+    public void MotherboardOn()
+    {
+        
+    }
+
+    public void CpuOn()
+    {
+
+    }
+
+    public void PowerOn()
+    {
+
+    }
+
+    public void PipeOn()
+    {
+
+    }
+
+    public void UsbOn()
+    {
+
+
+    }
+
+    public void ScreenOn()
+    {
+
+    }
+
+    public void CablesOn()
+    {
+
+    }
+
+
+
+
+
+}
